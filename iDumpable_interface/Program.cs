0addeebf7d1d2b4c945aa301cfd689bfdc6992b1
Christen Garcia <30610163+christengarcia@users.noreply.github.com>
2018-02-15 20:57:34 +1100

    Initial commit

---
+++ iDumpable_interface/Program.cs
@@ -0,0 +1,14 @@
+﻿/*
+ * C# Program to Demonstrate IDumpable Interface
+ */
+
+using System;
+
+namespace iDumpable_interface
+{
+    interface IDumpable
+    {
+        string Name { get; set; }
+        void Dump();
+    }
+}
